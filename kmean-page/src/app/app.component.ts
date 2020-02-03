7d0305adc8a087bd7c0217834795fb030fc7124e
morco <mor.cohen1989@gmail.com>
2020-02-03 12:55:50 +0200

    properly drawing

--- kmean-page/src/app/app.component.ts
+++ kmean-page/src/app/app.component.ts
@@ -4,6 +4,10 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { RestServiceService } from './rest.service';
 import { Subscription } from 'rxjs';
 
+const ROW_SIZE = 28;
+const COL_SIZE = 28;
+const CHANNELS = 4;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -28,5 +32,32 @@ export class AppComponent {
       this.clusters = (res as any).clusters;
       this.clusterLabels = Object.keys(this.clusters);
     })
+    document.getElementById("btn-results").style.display = 'block';
+  }
+
+  drawClusters = () => {
+    this.clusterLabels.forEach(label => {
+      this.drawImage('canvas-label-' + label, this.clusters[label].center);
+      this.clusters[label].randoms.forEach((random, i) => {
+        this.drawImage('canvas-random-' + label + '-' + i, random);
+      })
+    })
+  }
+
+  drawImage = (canvasId, imgArray) => {
+    let canvas: any = document.getElementById(canvasId);
+    let ctx = canvas.getContext('2d');
+    let imageData = ctx.createImageData(ROW_SIZE, COL_SIZE);
+
+    let data = imageData.data;
+
+    for (let i = 0; i < ROW_SIZE * COL_SIZE * CHANNELS; i += 4) {
+      data[i] = imgArray[i];
+      data[i + 1] = imgArray[i];
+      data[i + 2] = imgArray[i];
+      data[i + 3] = 255;
+    }
+
+    ctx.putImageData(imageData, 0, 0);
   }
 }
