784e9d303a42ecbf54a9acd99ad61f57c4ac003e
morco <mor.cohen1989@gmail.com>
2020-02-07 17:28:59 +0200

    rotated iamge, added .png writer for better debug

--- server/mnist-reader.js
+++ server/mnist-reader.js
@@ -1,34 +1,36 @@
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
+const PNG = require('pngjs').PNG;
 const dir = __dirname + path.sep + 'MNIST_DATASET' + path.sep;
-const IMAGE_LIM = 60000;
-const NUM_OF_ROWS = 28;
-const NUM_OF_COLUMNS = 28;
-const PIXEL_OFFEST = 15;
+const IMAGE_LIM = 100;
+const IMG_WIDTH = 28;
+const IMG_HEIGHT = 28;
+const PIXEL_OFFEST = 16;
 const ROW_OFFSET = 8;
 
 let dataFileBuffer = fs.readFileSync(dir + 'train-images.idx3-ubyte');
 let labelFileBuffer = fs.readFileSync(dir + 'train-labels.idx1-ubyte');
 let pixelValues = [];
 
+
 for (var image = 0; image < IMAGE_LIM; image++) {
     var pixels = [];
 
-    for (var x = 0; x < NUM_OF_ROWS; x++) {
-        for (var y = 0; y < NUM_OF_COLUMNS; y++) {
-            pixels.push(dataFileBuffer[(image * NUM_OF_ROWS * NUM_OF_COLUMNS) + (x + (y * NUM_OF_ROWS)) + PIXEL_OFFEST]);
+    for (var y = 0; y < IMG_HEIGHT; y++) {
+        for (var x = 0; x < IMG_WIDTH; x++) {
+            pixels.push(dataFileBuffer[(image * IMG_WIDTH * IMG_HEIGHT) + (x + (y * IMG_WIDTH)) + PIXEL_OFFEST]);
         }
     }
 
     var imageData = {};
     imageData[JSON.stringify(labelFileBuffer[image + ROW_OFFSET])] = pixels;
 
+
     pixelValues.push(imageData);
 }
 
 let groupedImages = {};
 let datasetArray = [];
-
 pixelValues.forEach(image => {
     for (let digit in image) {
         if (groupedImages[digit]) {
@@ -39,6 +41,29 @@ pixelValues.forEach(image => {
         datasetArray.push(image[digit]);
     }
 })
+
+let writePngImage = (source, name) => {
+    name = name || 'newOut.png';
+    var png = new PNG({
+        width: 28,
+        height: 28,
+        filterType: -1
+    })
+
+    for (let y = 0; y < png.height; y++) {
+        for (let x = 0; x < png.width; x++) {
+            let idx = (png.width * y + x) << 2;
+            let data = pngsource[idx];
+            png.data[idx] = data;
+            png.data[idx + 1] = data;
+            png.data[idx + 2] = data;
+            png.data[idx + 3] = data >> 1;
+        }
+    }
+    png.pack().pipe(fs.createWriteStream(name));
+}
+
+
 
 module.exports.DATASET = pixelValues;
 module.exports.DATASET_ARRAY = datasetArray;
